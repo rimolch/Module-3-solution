398a980a598c41505513b3c947a2d14fcc3d8a36
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-07-08 21:28:22 +0600

    Add files via upload

---
+++ module-3.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/script.js"></script>
+  <script type="text/javascript">
+  $(document).ready(function(){
+  $('[href=#wrp]').click(function(){
+  $('html,body').animate({scrollTop:0},'slow');
+  return false;});
+});
+  </script>
+
